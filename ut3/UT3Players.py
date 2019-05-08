7adc59c066373497b2239474ac0b78e40ee87b03
keeeal <james@keal.com.au>
2019-05-08 14:06:31 +0930

    implemented human player

--- ut3/UT3Players.py
+++ ut3/UT3Players.py
@@ -18,7 +18,7 @@ class HumanUT3Player():
     def play(self, board):
         valid = self.game.getValidMoves(board, 1)
         print('Valid moves:')
-        print(', '.join(str(int(i/self.game.n**2))+' '+str(int(i%self.game.n**2)) for i in valid if i)
+        print(', '.join(str(int(i/self.game.n**2))+' '+str(int(i%self.game.n**2)) for i in valid if i))
         while True:
             a = input()
             x, y = [int(x) for x in a.split(' ')]
